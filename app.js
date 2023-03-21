a4436c451cb95253ffb4f2e9928f223e82f55113
Hrishikesh Kumar <98553139+hrkhrishikesh@users.noreply.github.com>
2023-03-21 19:59:34 +0530

    changes in app.js
    removed {useUnifiedTopology} from mongo.connect

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb+srv://hrkblog:hrkblog@cluster0.tfm1t2p.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://hrkblog:hrkblog@cluster0.tfm1t2p.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true});
 
 const postSchema = {
   title: String,
